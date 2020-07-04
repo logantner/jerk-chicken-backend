5a0b83968a57a8db91734c025fdaf09bd70e7e70
James Bell <jsbell406@gmail.com>
2020-07-04 15:51:10 -0600

    added dev json token

--- jerk-chicken/src/main/java/com/jerk/chicken/services/UserService.java
+++ jerk-chicken/src/main/java/com/jerk/chicken/services/UserService.java
@@ -2,14 +2,18 @@ package com.jerk.chicken.services;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jerk.chicken.models.Recipe;
+import com.jerk.chicken.models.Role;
 import com.jerk.chicken.models.User;
 import com.jerk.chicken.models.UserRecipe;
 import com.jerk.chicken.repositories.UserRecipeRepository;
@@ -54,8 +58,22 @@ public class UserService {
 	private String generateJWT() {
 		Instant now = Instant.now();
 
+		List<Role> roles = new ArrayList<>();
+		roles.add(new Role(1,"admin"));
+		roles.add(new Role(2, "user"));
+		
+		String json = "";
+		
+		ObjectMapper om = new ObjectMapper();
+		try {
+			json = om.writeValueAsString(roles);
+		}catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		
 		byte[] secret = Base64.getDecoder().decode("m+xugUWvhJ2d7q6JoObRztTm19A4e9CrlqhWVn+JGQs=");
-		String jwt = Jwts.builder().claim("role", "user").claim("username", "username").setIssuedAt(Date.from(now))
+		String jwt = Jwts.builder().claim("roles", json).claim("username", "username").setIssuedAt(Date.from(now))
 				.setExpiration(Date.from(now.plus(24, ChronoUnit.HOURS))).signWith(SignatureAlgorithm.HS256, secret)
 				.compact();
 
