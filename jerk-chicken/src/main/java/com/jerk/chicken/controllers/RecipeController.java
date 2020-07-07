e46f722abd473ab8d16cf83d65a073e747dfa12d
Eric Last <elast27@yahoo.com>
2020-07-07 15:29:10 -0400

    converted recipe to RestController

--- jerk-chicken/src/main/java/com/jerk/chicken/controllers/RecipeController.java
+++ jerk-chicken/src/main/java/com/jerk/chicken/controllers/RecipeController.java
@@ -4,20 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.jerk.chicken.models.Recipe;
 import com.jerk.chicken.services.RecipeService;
 
-@Controller
+@RestController
 @RequestMapping("/recipes")
 public class RecipeController {
 	
@@ -26,27 +25,23 @@ public class RecipeController {
 	RecipeService rs;
 	
 	@GetMapping
-	@ResponseBody
 	public List<Recipe> getAllRecipes(){
 		return rs.getAllRecipes();
 	}
 	
 	@PostMapping
-	@ResponseBody
 	@ResponseStatus(HttpStatus.CREATED)
 	public Recipe addRecipe(@RequestBody Recipe r) {
 		return rs.addRecipe(r);
 	}
 	
 	@DeleteMapping
-	@ResponseBody
 	@ResponseStatus(HttpStatus.OK)
 	public void deleteRecipe(@RequestBody Recipe r) {
 		rs.deleteRecipe(r);
 	}
 	
 	@PutMapping
-	@ResponseBody
 	@ResponseStatus(HttpStatus.OK)
 	public Recipe updateRecipe(@RequestBody Recipe r) {
 		return rs.updateRecipe(r);
