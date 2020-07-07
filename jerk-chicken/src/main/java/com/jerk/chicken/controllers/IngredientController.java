aaa2212967aa40fc86d3f86c80dd36103c272fd7
James Bell <jsbell406@gmail.com>
2020-07-07 14:05:04 -0600

    cors fixed

--- jerk-chicken/src/main/java/com/jerk/chicken/controllers/IngredientController.java
+++ jerk-chicken/src/main/java/com/jerk/chicken/controllers/IngredientController.java
@@ -3,6 +3,7 @@ package com.jerk.chicken.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jerk.chicken.models.dto.IngredientBasketDTO;
 import com.jerk.chicken.services.IngredientService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/ingredients")
 public class IngredientController {
